afd990bb28193af20d660e6aade9ad5edaf7c254
Jadi Tharun Kumar <jaditharun@gmail.com>
2026-08-14 00:36:09 +0530

    3 commit

---
+++ src/practicePrograms/JavaTestClass.java
@@ -0,0 +1,16 @@
+package practicePrograms;
+import java.util.Scanner;
+
+public class JavaTestClass {
+
+	public static void main(String[] args) {
+		int num1 ,num2;
+		System.out.println("Enter first number: ");
+		Scanner sc = new Scanner(System.in);
+		num1 = sc.nextInt();
+		System.out.println("Enter second number: ");
+		num2 = sc.nextInt();
+		System.out.println("sum of two numbers: " + (num1+num2));
+	}
+
+}
